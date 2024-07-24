d9e5cd0d014309ead181b6bd3d42da4bfcbb0cc4
Amy Lyne <amy.lyne92@gmail.com>
2024-07-23 22:12:54 -0600

    Server Add on

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const Emotion = require('./models/emotion'); 
+const Emotion = require('./models/emotion');
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,42 +16,65 @@ mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-app.post('/emotions', async (req, res) => {
+app.post('/emotions', async (req, res, next) => {
   const emotion = new Emotion(req.body);
   try {
     await emotion.save();
-    res.send(emotion);
+    res.status(201).send(emotion);
   } catch (error) {
-    res.status(400).send(error);
+    next(error);
   }
 });
 
-app.get('/emotions', async (req, res) => {
-  const emotions = await Emotion.find();
-  res.send(emotions);
-});
-
-app.get('/emotions/:name', async (req, res) => {
-  const emotion = await Emotion.findOne({ name: req.params.name });
-  res.send(emotion);
-});
-
-app.put('/emotions/:name', async (req, res) => {
+app.get('/emotions', async (req, res, next) => {
   try {
-    const emotion = await Emotion.findOneAndUpdate({ name: req.params.name }, req.body, { new: true });
-    res.send(emotion);
+    const emotions = await Emotion.find();
+    res.send(emotions);
   } catch (error) {
-    res.status(400).send(error);
+    next(error);
   }
 });
 
-app.delete('/emotions/:name', async (req, res) => {
+app.get('/emotions/:name', async (req, res, next) => {
   try {
-    await Emotion.findOneAndDelete({ name: req.params.name });
+    const emotion = await Emotion.findOne({ name: req.params.name });
+    if (!emotion) {
+      return res.status(404).send({ error: 'Emotion not found' });
+    }
+    res.send(emotion);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.put('/emotions/:name', async (req, res, next) => {
+  try {
+    const emotion = await Emotion.findOneAndUpdate({ name: req.params.name }, req.body, { new: true, runValidators: true });
+    if (!emotion) {
+      return res.status(404).send({ error: 'Emotion not found' });
+    }
+    res.send(emotion);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete('/emotions/:name', async (req, res, next) => {
+  try {
+    const emotion = await Emotion.findOneAndDelete({ name: req.params.name });
+    if (!emotion) {
+      return res.status(404).send({ error: 'Emotion not found' });
+    }
     res.send({ message: 'Emotion deleted' });
   } catch (error) {
-    res.status(400).send(error);
+    next(error);
   }
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: err.message });
 });
 
 app.listen(PORT, () => {
